2d1389e293417af3f6f9659fcda74400bf0b4464
mairanymachado <mairanycm@gmail.com>
2019-10-07 21:01:28 -0300

    Hello SpringBoot and Rest

---
+++ 01 - restWithSpringBoot Inicio Project/src/main/java/br/com/erudio/Startup.java
@@ -0,0 +1,14 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
